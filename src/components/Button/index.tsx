01fe53a95edaebefc9dccf820e50f1226b0fc4aa
ssori <115159126+ssori0421@users.noreply.github.com>
2024-02-19 20:47:45 +0900

    chore: classnames 패키지 추가 및 css 확장을 위한 classnames 적용

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,17 @@
 import React, { ButtonHTMLAttributes } from 'react';
 import styels from './button.module.scss';
+import classNames from 'classnames';
 
 interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   icon?: JSX.Element;
-  children: JSX.Element;
+  children: React.ReactNode;
 }
 
 const Button = (props: IButtonProps) => {
-  const { icon, children, ...restProps } = props;
+  const { icon, children, className, ...restProps } = props;
 
   return (
-    <button {...restProps} className={styels.ButtonContainer}>
+    <button {...restProps} className={classNames(className, styels.ButtonContainer)}>
       {icon && icon}
       <span>{children}</span>
     </button>
