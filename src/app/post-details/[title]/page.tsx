f6f3ef5c309db0a0226e01ceff843430ba5da5d3
ssori <115159126+ssori0421@users.noreply.github.com>
2024-02-20 10:53:42 +0900

    feat: 자동 링크 생성을 위한 util 함수 생성 및 적용

--- src/app/post-details/[title]/page.tsx
+++ src/app/post-details/[title]/page.tsx
@@ -1,25 +1,40 @@
 'use client';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './postDetails.module.scss';
 import Button from '@/components/Button';
 import MarkdownViewer from '@/components/MarkdownViewer';
-import { GET_POST, selectCurrentPost } from '@/redux/postSlice';
+import { GET_POST, selectCurrentPost, selectPosts } from '@/redux/postSlice';
 import Link from 'next/link';
 import { useDispatch, useSelector } from 'react-redux';
+import { replaceContentHtmlWordToLink } from '@/utils/text';
+import dayjs from 'dayjs';
 
-const PostDetailPage = ({ params }: { params: { title: string } }) => {
+const PostDetailPage = (props: { params: { title: string } }) => {
+  const [currentUrl, setCurrentUrl] = useState('');
+
   const dispatch = useDispatch();
-  const currentPostParam = decodeURIComponent(params.title);
+  const currentPostParam = decodeURIComponent(props.params.title);
 
-  const data = useSelector(selectCurrentPost);
+  console.log('currentPostParam', currentPostParam);
+
+  const posts = useSelector(selectPosts);
+  const currentPost = useSelector(selectCurrentPost);
+
+  useEffect(() => {
+    const currentUrl = window.location.origin;
+    setCurrentUrl(currentUrl);
+  }, []);
 
   useEffect(() => {
     dispatch(GET_POST({ title: currentPostParam }));
   }, [currentPostParam, dispatch]);
 
-  if (!data) return null;
-
-  console.log({ data });
+  const newPostContent = replaceContentHtmlWordToLink(
+    currentPost.html,
+    posts,
+    currentUrl,
+    currentPostParam
+  );
 
   return (
     <>
@@ -29,9 +44,12 @@ const PostDetailPage = ({ params }: { params: { title: string } }) => {
         </Button>
       </div>
       <div className={styles.postDetailsContainer}>
-        <h1>{data.title}</h1>
-
-        <MarkdownViewer postData={data?.content} />
+        <h1>{currentPost.title}</h1>
+        <div className={styles.dateWrapper}>
+          <p>최초 작성 시간 : {dayjs(currentPost.created_at).format('YYYY-MM-DD HH:mm:ss')}</p>
+          <p>최근 수정 시간 : {dayjs(currentPost.updated_at).format('YYYY-MM-DD HH:mm:ss')}</p>
+        </div>
+        <MarkdownViewer postData={newPostContent} />
       </div>
     </>
   );
--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,3 +1,6 @@
+import { IPost } from '@/types/post';
+import * as cheerio from 'cheerio';
+
 export const truncateText = (inputText: string, maxLength: number = 10) => {
   if (inputText.length <= maxLength) {
     return inputText;
@@ -16,4 +19,36 @@ export const convertMarkdownToPlainText = (markdownText: string) => {
     .replace(/(?:__|[*#])|\*|\[.*?\]/g, '') // 볼드체, 이탤릭체, 기타 서식 제거
     .replace(/\n/g, ' ') // 줄 바꿈을 공백으로 대체
     .trim(); // 문자열 양 끝 공백 제거
+};
+
+export const replaceContentHtmlWordToLink = (
+  htmlText: string,
+  posts: IPost[],
+  currentUrl: string,
+  excludeText?: string
+): string => {
+  const $ = cheerio.load(htmlText, { xmlMode: true });
+
+  const filteredPosts = !!excludeText ? posts.filter((post) => post.title !== excludeText) : posts;
+  console.log(filteredPosts);
+
+  for (const post of filteredPosts) {
+    const urlMarkdown = `<a href="${currentUrl}/post-details/${post.title}">${post.title}</a>`;
+
+    $(`*:contains('${post.title}')`).each((index, element) => {
+      const $element = $(element);
+
+      const node = $element.contents().filter(function () {
+        return this.nodeType === 3;
+      });
+
+      const content = node.text();
+
+      if (content.includes(post.title)) {
+        node.replaceWith(content.replace(new RegExp(post.title, 'g'), urlMarkdown));
+      }
+    });
+  }
+
+  return $.html();
 };
